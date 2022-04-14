6858d8fdcd388df94a8b5f84c2496be19c840d91
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-14 21:12:24 +0900

    application init

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import './reset.css';
+import './app.css';
+
+const root = document.querySelector('#root');
+root.innerHTML = `Hello world`;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const nodeEnv = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: nodeEnv === "development" ? "development" : "production",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          nodeEnv === "development"
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+              },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 20000,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
