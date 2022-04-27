01bd38f52d4ebcb3d9beacd848171bc6edd63b92
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-27 15:17:03 +0900

    refactor

--- src/components/imgContents.js
+++ src/components/imgContents.js
@@ -1,14 +1,19 @@
 import faceImg from '../imgs/face.png';
 import footerImg from '../imgs/footer.png';
 import flowersImg from '../imgs/flowers.png';
+import jsHeartImg from '../imgs/js-heart.png';
 const earthImg =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/1280px-The_Blue_Marble_%28remastered%29.jpg';
-const hardImg =
-  'https://velog.velcdn.com/images/jiho3894/post/ea065fbc-8730-47c8-937f-d634cef354a3/image.gif';
+const clothes1Img =
+  'https://cdn.shopify.com/s/files/1/0587/2605/6143/products/DSN-Logo-Tee-White5_1080x.jpg?v=1646380431';
+const hard1Img =
+  'https://velog.velcdn.com/images%2Fkim-taewoo%2Fpost%2Fb8ee8d29-42a7-4f7a-85d0-93698a2ac87b%2Ffail.gif';
 export const imgContents = {
   1: faceImg,
   2: footerImg,
   3: flowersImg,
   4: earthImg,
-  5: hardImg,
+  5: clothes1Img,
+  6: hard1Img,
+  7: jsHeartImg,
 };
