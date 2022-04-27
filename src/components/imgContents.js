66fd567dc6c40785427a70c8c53ad57f51621a4e
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-27 14:49:49 +0900

    refactor

--- src/components/imgContents.js
+++ src/components/imgContents.js
@@ -1,9 +1,14 @@
 import faceImg from '../imgs/face.png';
 import footerImg from '../imgs/footer.png';
 import flowersImg from '../imgs/flowers.png';
-
+const earthImg =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/1280px-The_Blue_Marble_%28remastered%29.jpg';
+const hardImg =
+  'https://velog.velcdn.com/images/jiho3894/post/ea065fbc-8730-47c8-937f-d634cef354a3/image.gif';
 export const imgContents = {
   1: faceImg,
   2: footerImg,
   3: flowersImg,
+  4: earthImg,
+  5: hardImg,
 };
