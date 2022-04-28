e16f506bf7bc824903b9009c200f5081b08b5e81
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-28 17:09:26 +0900

    feat: process images with canvas api

--- src/components/imgContents.js
+++ src/components/imgContents.js
@@ -1,6 +1,6 @@
-import faceImg from '../imgs/face.png';
-import footerImg from '../imgs/footer.png';
-import flowersImg from '../imgs/flowers.png';
+import faceImg from '../imgs/face.jpg';
+import footerImg from '../imgs/footer.jpg';
+import flowersImg from '../imgs/flowers.jpg';
 const earthImg =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/1280px-The_Blue_Marble_%28remastered%29.jpg';
 const clothes1Img =
@@ -11,11 +11,6 @@ const love1Img =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTAWduqkj6SPVIQe32haDaXg_mgSBIxGq1xA&usqp=CAU';
 const ryanDahlImg =
   'http://berb.github.io/nodejs-talk-chaosseminar/assets/images/ryah.jpg';
-const dokdoImg =
-  'http://newsimg.hankookilbo.com/2019/09/25/201909251098716471_2.jpg';
-const js1Img =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png';
-
 export const imgContents = [
   faceImg,
   footerImg,
@@ -25,6 +20,4 @@ export const imgContents = [
   hard1Img,
   love1Img,
   ryanDahlImg,
-  dokdoImg,
-  js1Img,
 ];
