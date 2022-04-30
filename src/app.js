321f59fd62950ad4299996f755277b218a9ff6a8
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-30 10:31:36 +0900

    feat: DOMContentLoaded added

--- src/app.js
+++ src/app.js
@@ -4,28 +4,30 @@ import './routes/content.css';
 import { router } from './components/router.js';
 import { routeContents } from './components/routeContents.js';
 
-document.body.addEventListener('click', e => {
-  if (e.target.localName === 'a') {
-    if (
-      ((routeContents[
-        e.target.href.substring(
-          e.target.href.indexOf('/', 8),
-          e.target.href.length
-        )
-      ] ===
-        undefined) ===
-        false) ===
-      true
-    ) {
-      e.preventDefault();
-      router(
-        e.target.href.substring(
-          e.target.href.indexOf('/', 8),
-          e.target.href.length
-        )
-      );
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', e => {
+    if (e.target.localName === 'a') {
+      if (
+        ((routeContents[
+          e.target.href.substring(
+            e.target.href.indexOf('/', 8),
+            e.target.href.length
+          )
+        ] ===
+          undefined) ===
+          false) ===
+        true
+      ) {
+        e.preventDefault();
+        router(
+          e.target.href.substring(
+            e.target.href.indexOf('/', 8),
+            e.target.href.length
+          )
+        );
+      }
+      return;
     }
-    return;
-  }
+  });
+  router(window.location.pathname);
 });
-router(window.location.pathname);
